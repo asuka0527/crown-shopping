688c1cc213797eee12a8118d506485986316bca6
asuka0527 <junizzanakajima@gmail.com>
2021-01-06 12:21:55 +0900

    cartIcon and cartDropdown components and implemented cartReducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,9 @@
 import { combineReducers } from "redux";
 
 import userReducer from "./user/user.reducer";
-
+import cartReducer from "./cart/cart.reducer";
 // root reducer -  overall reducer
 export default combineReducers({
   user: userReducer,
+  cart: cartReducer,
 });
